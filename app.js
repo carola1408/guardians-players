ec7a72350a6f912f7142a744e0b1f6fb37e1d081
user <carola1408@yahoo.com.tw>
2023-07-26 16:22:39 +0800

    feat: connect mongodb

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const mongoose = require('mongoose') // 載入 mongoose
+
 const app = express()
+mongoose.connect(process.env.MONGODB_URI) // 設定連線到 mongoDB mongoose.connect 是 Mongoose 提供的方法，
 
 
 // 設定首頁路由
