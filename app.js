3e5f8c6b9abab659b2999fad2970b0853007b819
user <carola1408@yahoo.com.tw>
2023-07-26 16:49:02 +0800

    feat: mongodb connected successly

--- app.js
+++ app.js
@@ -8,7 +8,18 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const app = express()
-mongoose.connect(process.env.MONGODB_URI) // 設定連線到 mongoDB mongoose.connect 是 Mongoose 提供的方法，
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB mongoose.connect 是 Mongoose 提供的方法
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 
 // 設定首頁路由
