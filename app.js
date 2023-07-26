0683d239a46527b662f53966950091b94416a2c0
user <carola1408@yahoo.com.tw>
2023-07-26 16:40:13 +0800

    feat: install dotenv

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
 
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
 mongoose.connect(process.env.MONGODB_URI) // 設定連線到 mongoDB mongoose.connect 是 Mongoose 提供的方法，
 
